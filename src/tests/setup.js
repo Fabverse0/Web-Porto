7dfd5f603fd399704a6c91e524cd820cb06e1409
Fabian <mfabian.rizky@gmail.com>
2026-08-11 19:47:38 +0700

    test: add IntersectionObserver mock in setup.js for jsdom environment compatibility

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom';
+import { vi } from 'vitest';
+
+class IntersectionObserverMock {
+  constructor() {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+window.IntersectionObserver = IntersectionObserverMock;
+global.IntersectionObserver = IntersectionObserverMock;
